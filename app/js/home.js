4dc9bca5ff0f8672806f97894864fb22a80d960a
Jared Crystal <jareditesrock@gmail.com>
2015-12-02 21:37:12 -0700

    added api.js

--- app/js/home.js
+++ app/js/home.js
@@ -1,4 +1,5 @@
 var ActivityList = require("./activityList.js");
+var api = require(".api.js");
 
 
 var DATA = [
@@ -185,29 +186,5 @@ var Home = React.createClass({
       );
   }
 })
-
-// API object
-var api = {
-    // get the list of activities, call the callback when complete
-    getItems: function(cb) {
-        var url = "/api/activities";
-        $.ajax({
-            url: url,
-            dataType: 'json',
-            type: 'GET',
-            //headers: {'Authorization': localStorage.token},
-            success: function(res) {
-                if (cb)
-                    cb(true, res);
-            },
-            error: function(xhr, status, err) {
-                // if there is an error, remove the login token
-                delete localStorage.token;
-                if (cb)
-                    cb(false, status);
-            }
-        });
-    }
-};
 
 module.exports = Home;
