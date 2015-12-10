3d2557f90d40c52a188c89aeea3c4006e94f71f4
Nathan Tylka <tylkan@localhost.localdomain>
2015-12-10 14:52:09 -0700

    confirmation of creation or error

--- app/js/createactivity.js
+++ app/js/createactivity.js
@@ -1,45 +1,56 @@
 var API = require("./api.js");
+var Login = require("./login.js");
 
 var CreateActivity = React.createClass({
 
     getInitialState: function() {
         return {
             // there was an error on logging in
-            error: false
+            error: false,
+            success: false
         };
 
     },
 
     createActivity: function(event){
         event.preventDefault();
-
-        var title = this.refs.title.value;
-        var tags = this.refs.tags.value.split(/[ ,]+/);
-        var description = this.refs.description.value;
-        var cost = this.refs.cost.value;
-        var address = this.refs.address.value;
-        var activity = {};
-        activity.title = title;
-        activity.tags = tags;
-        activity.description = description;
-        activity.price = cost;
-        activity.address = address;
-        activity.comments = [];
-        API.createActivity(activity, function(submitSucceeded){
-            if (submitSucceeded){
-                this.forceUpdate();
-            }
-            else{
-                return this.setState({
-                    error: true
-                });
-            }
-        }.bind(this));
+        if  (!!localStorage.token){
+            var title = this.refs.title.value;
+            var tags = this.refs.tags.value.split(/[ ,]+/);
+            var description = this.refs.description.value;
+            var cost = this.refs.cost.value;
+            var address = this.refs.address.value;
+            var activity = {};
+            activity.title = title;
+            activity.tags = tags;
+            activity.description = description;
+            activity.price = cost;
+            activity.address = address;
+            activity.comments = [];
+            API.createActivity(activity, function(submitSucceeded){
+                if (submitSucceeded){
+                    this.refs.title.value = "";
+                    this.refs.description.value = "";
+                    this.refs.tags.value = "";
+                    this.refs.cost.value = "";
+                    this.refs.address.value = "";
+                    this.forceUpdate();
+                    this.setState({error: false, success: true})
+                }
+                else{
+                    return this.setState({
+                        error: true,
+                        success: false
+                    });
+                }
+            }.bind(this));
+        }else{
+            alert("You must be logged in to create an activity");
+        }
     },
 
   render: function(){
     return (
-
       <div className="container">
         <div className="row">
           <div className="col-md-6 col-md-offset-3">
@@ -72,11 +83,16 @@ var CreateActivity = React.createClass({
                             <br/>
                             <div className="input-group">
   								<span className="input-group-addon" id="Address-addon">Address</span>
-                                <input type="text" className="form-control" id="ActivityAddress" placeholder="Activity Adress" ref="address"/>
+                                <input type="text" className="form-control" id="ActivityAddress" placeholder="Activity Address" ref="address"/>
                             </div>
                             <br/>
                             <button type="submit" className="btn btn-warning">Submit</button>
+                            
                         </form>
+                        {this.state.success ? (<br/>) : null}
+                            {this.state.success ? (<h4><span className="label label-info">Added the new activity</span></h4>) : null}
+                            {this.state.error ? (<br/>) : null}
+                            {this.state.error ? (<h4><span className="label label-danger">Something went wrong</span></h4>) : null}
                     </div>
                 </div>
             </div>
