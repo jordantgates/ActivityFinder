3496d16ca9876d4bcc10a0440e8802debbe330e0
Jordan Gates <jordangates@Jordans-MacBook-Pro.local>
2016-06-24 11:37:08 -0600

    updating

--- gulpFile.js
+++ gulpFile.js
@@ -1,18 +1,18 @@
-var gulp = require('gulp')
+var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var reactify = require('reactify');
 
 
 gulp.task('browserify', function () {
-  var b = browserify();
-  b.transform(reactify);
-  b.add('./app/js/app.js'); // The root file
-  return (
-    b.bundle()
-      .pipe(source('output.js')) // What the output file should be named
-      .pipe(gulp.dest('./app/__build__'))  // And what directory it should go to
-  );
+    var b = browserify();
+    b.transform(reactify);
+    b.add('./app/js/app.js'); // The root file
+    return (
+      b.bundle()
+        .pipe(source('output.js')) // What the output file should be named
+        .pipe(gulp.dest('./app/__build__'))  // And what directory it should go to
+        );
 });
 
 
